5da3d770a9fb087fcea1cd478c70c93146c51d29
rfce <rajsrivastava@tutanota.com>
2025-12-06 14:48:19 +0530

    ip address and user-agent in keep-alive

--- controllers/Author.js
+++ controllers/Author.js
@@ -27,6 +27,15 @@ const uploadImage = async (req, res) => {
 }
 
 const keepAlive = async (req, res) => {
+    const ua = req.get('User-Agent')
+
+    // Get the IP Address
+    // We check 'x-forwarded-for' first because if you are on a cloud host, 
+    // req.ip often returns the internal load balancer IP, not the real user.
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.ip
+
+    console.log(`+ Ping received from: ${ip}\n+ Client device: ${ua}`)
+
     res.json({
         success: true,
         message: "Web Service active"
